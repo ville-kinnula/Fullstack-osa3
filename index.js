278cf6c17db771ac364d4ff17c9ada41a19a6e30
Ville Kinnula <kinnula.ville@gmail.com>
2020-08-06 18:35:37 +0300

    uibuild

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time msy :
 app.use(express.static('build'))
 
 
-/* let persons = [
+let persons = [
   { name: 'Arto Hellas', number: '040-123456', id: 1 },
   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
   { name: 'Mary Poppendieck', number: '39-23-6423122', id:4 }
-] */
+] 
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
